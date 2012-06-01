1809605cdc44a8628508f8d5be89e0e734209fc3
Adam Coffman <acoffman@genome.wustl.edu>
2012-06-01 12:35:37 -0500

    code version 2.0.3

--- src/align_status.cpp
+++ src/align_status.cpp
@@ -20,54 +20,259 @@
 #include "bwt_map.h"
 #include "tokenize.h"
 #include "reads.h"
+#include "junctions.h"
+#include "insertions.h"
+#include "deletions.h"
+#include "fusions.h"
+#include "coverage.h"
 #include "align_status.h"
 
 using namespace std;
 
 AlignStatus::AlignStatus()
 {
-  _aligned = false;
-  _indelFreeAlignment = false;
-  _unannotatedSpliceFreeAlignment = false;
-  _edit_dist = 0xFF;
+  _alignment_score = std::numeric_limits<int>::min();
 } 
 
 /**
  * Parse the cigar string of a BowtieHit in order to determine the alignment status.
  */
-AlignStatus::AlignStatus(const BowtieHit& bh, const JunctionSet& gtf_junctions)
+AlignStatus::AlignStatus(const BowtieHit& bh,
+			 const JunctionSet& gtf_junctions,
+			 const JunctionSet& junctions,
+			 const InsertionSet& insertions,
+			 const DeletionSet& deletions,
+			 const FusionSet& fusions,
+			 const Coverage& coverage)
 {
+  // it seems like we need to test this more
+  // daehwan - it doesn't seem to work.
+  const bool recalculate_indel_score = false;
+  
   const vector<CigarOp>& cigar = bh.cigar();
-  _aligned = cigar.size() > 0;
-  _indelFreeAlignment = true;
-  _unannotatedSpliceFreeAlignment = true;
-  _edit_dist = bh.edit_dist();
+  _alignment_score = bh.alignment_score();
+
+  const int read_len = bh.read_len();
+  const int min_extent = min(read_len / 4, 10);
+  bool recalculate_score = !junctions.empty();
+
   int j = bh.left();
+  int r = 0;
+  RefID ref_id = bh.ref_id();
   for (size_t c = 0 ; c < cigar.size(); ++c)
     {
-      Junction junc;
-      switch(cigar[c].opcode)
+      int opcode = cigar[c].opcode;
+      int length = cigar[c].length;
+      switch(opcode)
 	{
 	case REF_SKIP:
-	  junc.refid = bh.ref_id();
-	  junc.left = j;
-	  junc.right = junc.left + cigar[c].length;
-	  junc.antisense = bh.antisense_splice();
-	  j += cigar[c].length;
+	case rEF_SKIP:
+	  {
+	    Junction junc;
+	    junc.refid = bh.ref_id();
+	    
+	    if (opcode == REF_SKIP)
+	      {
+		junc.left = j - 1;
+		junc.right = j + length;
+		j += length;
+	      }
+	    else
+	      {
+		junc.right = j + 1;
+		junc.left = j - length;
+		j -= length;
+	      }
 
-	  if (gtf_junctions.find(junc) == gtf_junctions.end())
-	    _unannotatedSpliceFreeAlignment = false;
-	  break;	  
+	    if (recalculate_score)
+	      {
+		junc.antisense = bh.antisense_splice();
+		
+		if (gtf_junctions.find(junc) == gtf_junctions.end())
+		  {
+		    JunctionSet::const_iterator itr = junctions.find(junc);
+		    if (itr == junctions.end())
+		      {
+			_alignment_score -= bowtie2_max_penalty;
+		      }
+		    else
+		      {
+			const int left_cov = coverage.get_coverage(ref_id, junc.left + 1);
+			const int right_cov = coverage.get_coverage(ref_id, junc.right - 1);
+			const int avg_cov = (left_cov + right_cov) / 2;
+			
+			int penalty = bowtie2_max_penalty + 2;
+			const int supporting_hits = itr->second.supporting_hits;
+			const int left_extent = itr->second.left_extent;
+			const int right_extent = itr->second.right_extent;
+			float extent_penalty = 0.0f;
+			if (left_extent < min_extent || right_extent < min_extent)
+			  extent_penalty = 0.5f;
+			
+			if (supporting_hits >= 5)
+			  penalty *= min((float)avg_cov/supporting_hits + extent_penalty, 1.f);
+
+			// add two points to prefer junction alignments to other that may span one side of split site.
+			penalty -= 2;
+
+			if (itr->second.gtf_match)
+			  penalty -= bowtie2_max_penalty;
+
+			int prev_alignment_score = _alignment_score;
+			_alignment_score -= penalty;
+
+			// daehwan - for debugging purposes
+			if (bh.insert_id() == 325708 && false)
+			  {
+			    fprintf(stderr, "junc(%d:%d-%d) %d / (%d + %d) = %d => %d\n",
+				    junc.refid, junc.left, junc.right,
+				    itr->second.supporting_hits, left_cov, right_cov,
+				    prev_alignment_score, _alignment_score);
+			    fprintf(stderr, "\textent: %d-%d\n",
+				left_extent, right_extent);
+			  }
+		      }
+		  }
+	      }
+	  }
+	  break;
+	  
 	case MATCH:
-	  j += cigar[c].length;
+	case mATCH:
+	  {
+	    if (opcode == MATCH)
+	      j += length;
+	    else
+	      j -= length;
+
+	    r += length;
+	  }
 	  break;
+	  
 	case DEL:
-	  j += cigar[c].length;
-	  _indelFreeAlignment = false;
+	case dEL:
+	  {
+	    Deletion deletion;
+	    deletion.refid = bh.ref_id();
+	    if (opcode == DEL)
+	      {
+		deletion.left = j - 1;
+		deletion.right = j + length;
+		j += length;
+	      }
+	    else
+	      {
+		deletion.right = j + 1;
+		deletion.left = j - length;
+		j -= length;
+	      }
+
+	    if (recalculate_score && recalculate_indel_score)
+	      {
+		DeletionSet::const_iterator itr = deletions.find(deletion);
+		if (itr != deletions.end())
+		  {
+		    const int left_cov = coverage.get_coverage(ref_id, deletion.left + 1);
+		    const int right_cov = (length == 1 ? left_cov : coverage.get_coverage(ref_id, deletion.right - 1));
+		    const int avg_cov = (left_cov + right_cov) / 2;
+		    const int del_penalty = bowtie2_ref_gap_open + bowtie2_ref_gap_cont * length;
+		    int addition = del_penalty;
+		    
+		    const int supporting_hits = itr->second.supporting_hits;
+		    const int left_extent = itr->second.left_extent;
+		    const int right_extent = itr->second.right_extent;
+		    int penalty = 0;
+		    if (left_extent < min_extent || right_extent < min_extent)
+			  penalty = del_penalty * 0.5f;
+			
+		    if (avg_cov > 0 && supporting_hits >= 5)
+		      addition *= min((float)supporting_hits/avg_cov, 1.f);
+		    else
+		      addition = 0;
+
+		    addition -= penalty;
+		    if (addition < 0)
+		      addition = 0;
+
+		    int prev_alignment_score = _alignment_score;
+		    _alignment_score += addition;
+		    _alignment_score = min(0, _alignment_score);
+
+		    // daehwan - for debug purposes
+		    if (bh.insert_id() == 325708 && false)
+		      {
+			fprintf(stderr, "del(%d:%d-%d) %d / (%d + %d) = %d => %d\n",
+				deletion.refid, deletion.left, deletion.right,
+				supporting_hits, left_cov, right_cov,
+				prev_alignment_score, _alignment_score);
+			fprintf(stderr, "\textent: %d-%d\n",
+				left_extent, right_extent);
+		      }
+		  }
+	      }
+	  }
 	  break;
+
 	case INS:
-	  _indelFreeAlignment = false;
+	case iNS:
+	  {
+	    if (recalculate_score && recalculate_indel_score)
+	      {
+		string seq = bh.seq().substr(r, length);
+		Insertion ins(ref_id, j, seq);
+		InsertionSet::const_iterator itr = insertions.find(ins);
+		if (itr != insertions.end())
+		  {
+		    const int supporting_hits = itr->second.supporting_hits;
+		    const int left_extent = itr->second.left_extent;
+		    const int right_extent = itr->second.right_extent;
+		    
+		    const int left_cov = coverage.get_coverage(ref_id, j);
+		    const int right_cov = coverage.get_coverage(ref_id, j + (opcode == INS ? 1 : -1));
+		    const int avg_cov = (left_cov + right_cov) / 2 - supporting_hits;
+		    const int ins_penalty = bowtie2_read_gap_open + bowtie2_read_gap_cont * length;
+		    int addition = ins_penalty;
+		    int extent_penalty = 0.0f;
+		    if (left_extent < min_extent || right_extent < min_extent)
+		      extent_penalty = ins_penalty * 0.5f;
+		    
+		    if (avg_cov > 0 && supporting_hits >= 5)
+		      addition *= min((float)supporting_hits/avg_cov, 1.f);
+		    else
+		      addition = 0;
+
+		    addition -= extent_penalty;
+		    if (addition < 0)
+		      addition = 0;
+
+		    // int prev_alignment_score = _alignment_score;
+		    _alignment_score += addition;
+		    _alignment_score = min(0, _alignment_score);
+		    
+		    /*
+		      fprintf(stderr, "ins(%d:%d:%s) %d / (%d - %d) = %d => %d (%d)\n",
+		      ref_id, ins.left, seq.c_str(),
+		      supporting_hits, avg_cov, supporting_hits,
+		      prev_alignment_score, _alignment_score, ins_penalty);
+		      fprintf(stderr, "\textent: %d-%d\n",
+		      left_extent, right_extent);
+		    */
+		  }
+	      }
+
+	    r += length;
+	  }
 	  break;
+	  
+	case FUSION_FF:
+	case FUSION_FR:
+	case FUSION_RF:
+	case FUSION_RR:
+	  // daehwan - implement this later
+	  j = length;
+	  ref_id = bh.ref_id2();
+	  break;
+	  
 	default:
 	  break;
 	}
@@ -85,17 +290,10 @@ AlignStatus::AlignStatus(const BowtieHit& bh, const JunctionSet& gtf_junctions)
  */
 bool AlignStatus::operator<(const AlignStatus& rhs) const
 {
-  if (rhs._aligned != _aligned) return rhs._aligned;
-  if (rhs._edit_dist!=_edit_dist)
-      return rhs._edit_dist < _edit_dist;
-  //int lhs_value = _aligned ? 1 : 0;
-	int lhs_value = _indelFreeAlignment ? 4 : 0;
-	lhs_value += _unannotatedSpliceFreeAlignment ? 2 : 0;
+  if (_alignment_score != rhs._alignment_score)
+    return _alignment_score > rhs._alignment_score;
 
-	//int rhs_value = rhs._aligned ? 1 : 0;
-	int rhs_value = rhs._indelFreeAlignment ? 4 : 0;
-	rhs_value += rhs._unannotatedSpliceFreeAlignment ? 2 : 0;
-	return lhs_value < rhs_value;
+  return false;
 }
 
 /**
@@ -103,15 +301,10 @@ bool AlignStatus::operator<(const AlignStatus& rhs) const
  */
 bool AlignStatus::operator==(const AlignStatus& rhs) const
 {
-	return ((_aligned == rhs._aligned) && (rhs._edit_dist ==_edit_dist) &&
-		(_indelFreeAlignment == rhs._indelFreeAlignment) &&
-		(_unannotatedSpliceFreeAlignment == rhs._unannotatedSpliceFreeAlignment));
+  return _alignment_score == rhs._alignment_score;
 }
 
 bool AlignStatus::operator!=(const AlignStatus& rhs) const
 {
-	return !((*this) == rhs);
+  return _alignment_score != rhs._alignment_score;
 }
-
-
-
--- src/common.h
+++ src/common.h
@@ -19,12 +19,30 @@
 #include "bam/sam.h"
 
 
+#define MAX_READ_LEN 1024
+
+//for fastq and bam indexing by read# (multi-threading)
+#define INDEX_REC_COUNT 1000
+
 #define VMAXINT32 0xFFFFFFFF
 
 #ifdef MEM_DEBUG
  void process_mem_usage(double& vm_usage, double& resident_set);
  void print_mem_usage();
 #endif
+
+extern bool bowtie2;
+extern int bowtie2_min_score;
+extern int bowtie2_max_penalty;
+extern int bowtie2_min_penalty;
+extern int bowtie2_penalty_for_N;
+extern int bowtie2_read_gap_open;
+extern int bowtie2_read_gap_cont;
+extern int bowtie2_ref_gap_open;
+extern int bowtie2_ref_gap_cont;
+
+// daehwan - temporary for parallelization
+extern bool parallel;
 
 /*
  * Maximum allowable length of an
@@ -42,7 +60,7 @@ extern unsigned int max_deletion_length;
 
 extern int inner_dist_mean;
 extern int inner_dist_std_dev;
-extern int max_mate_inner_dist; 
+extern int max_mate_inner_dist;
 
 extern int min_anchor_len;
 extern int min_report_intron_length;
@@ -59,7 +77,7 @@ extern int max_segment_intron_length;
 
 extern uint32_t min_closure_exon_length;
 extern int island_extension;
-extern int num_cpus;
+extern int num_threads;
 extern int segment_length; // the read segment length used by the pipeline
 extern int segment_mismatches;
 extern int max_read_mismatches;
@@ -71,6 +89,7 @@ extern ReadFormat reads_format;
 
 extern bool verbose;
 extern unsigned int max_multihits;
+extern unsigned int max_seg_multihits;
 extern bool no_closure_search;
 extern bool no_coverage_search;
 extern bool no_microexon_search;
@@ -87,14 +106,18 @@ extern std::string sam_header;
 extern std::string sam_readgroup_id;
 extern std::string zpacker; //path to program to use for de/compression (gzip, pigz, bzip2, pbzip2)
 extern std::string samtools_path; //path to samtools executable
+extern std::string std_outfile; //main output file that some modules can use instead of stdout
 extern std::string aux_outfile; //auxiliary output file name
+extern std::string index_outfile; //index output file name
 extern bool solexa_quals;
 extern bool phred64_quals;
 extern bool quals;
 extern bool integer_quals;
 extern bool color;
-extern bool color_out;
 extern std::string gtf_juncs;
+
+extern bool report_secondary_alignments;
+extern bool report_discordant_pair_alignments;
 
 //prep_reads only: --flt-reads <bowtie-fastq_for--max>
 //  filter out reads if their numeric ID is in this fastq file
@@ -106,6 +129,19 @@ extern std::string flt_reads;
 //is NOT found in the flt_reads file, and write them into
 // aux_outfile; also reverses the flt_reads filter itself
 extern std::string flt_mappings;
+
+//for on-the-fly search during pre-filtering of PE reads, prep_reads will take both mates as input
+//but output only one side to stdout (into Bowtie); 0 = left, 1 = right, 2 = both
+extern int flt_side;
+
+extern bool fusion_search;
+extern size_t fusion_anchor_length;
+extern size_t fusion_min_dist;
+extern size_t fusion_read_mismatches;
+extern size_t fusion_multireads;
+extern size_t fusion_multipairs;
+extern std::vector<std::string> fusion_ignore_chromosomes;
+extern bool fusion_do_not_resolve_conflicts;
 
 enum eLIBRARY_TYPE
   {
@@ -124,8 +160,11 @@ enum eLIBRARY_TYPE
 
 extern eLIBRARY_TYPE library_type;
 std::string getFext(const std::string& s); //returns file extension converted to lowercase
+std::string getFdir(const std::string& s); //returns file extension converted to lowercase
 bool str_endsWith(std::string& str, const char* suffix);
-void str_appendInt(std::string& str, int v);
+void str_appendInt(std::string& str, int64_t v);
+void str_appendUInt(std::string& str, uint64_t v);
+std::string str_replace(const std::string& str, const std::string& oldStr, const std::string& newStr);
 FILE* fzOpen(std::string& fname, const char* mode);
 
 int parseIntOpt(int lower, const char *errmsg, void (*print_usage)());
@@ -134,7 +173,7 @@ int parse_options(int argc, char** argv, void (*print_usage)());
 void err_exit(const char* format,...); // exit with an error
 
 char* get_token(char** str, const char* delims);
-std::string guess_packer(const std::string& fname, bool use_all_cpus);
+std::string guess_packer(const std::string& fname, bool use_all_cpus=false);
 std::string getUnpackCmd(const std::string& fname, bool use_all_cpus=false);
 
 void checkSamHeader();
@@ -142,29 +181,56 @@ void writeSamHeader(FILE* fout);
 
 class FZPipe {
  public:
-	 FILE* file;
+	 union {
+	   FILE* file;
+	   samfile_t* bam_file;
+	 };
 	 std::string filename;
 	 std::string pipecmd;
 	 bool is_bam;
-	 FZPipe(std::string& fname, bool is_mapping):filename(fname),pipecmd() {
+	 FZPipe(const std::string& fname, bool guess=false):filename(fname),pipecmd() {
 	   //this constructor is only to use FZPipe as a READER
-       //also accepts/recognizes BAM files
-	   //for which it only stores the filename, other fields/methods are unused
-	   openRead(fname, is_mapping);
+       //also accepts/recognizes BAM files (without needing pipes)
+	   //for which it only stores the filename
+	   openRead(fname, guess);
 	   }
 
-   void openRead(std::string& fname, bool is_mapping) {
+   FILE* openRead(const std::string& fname, bool guess=false) {
      filename=fname;
      pipecmd="";
      is_bam=false;
-     if (is_mapping && getFext(fname) == "bam") {
-           file=(FILE*)this;
+     if (getFext(fname) == "bam") {
+           //file=(FILE*)this; //just to be non-NULL;
            is_bam=true;
-           return;
+           bam_file=samopen(filename.c_str(), "rb", 0);
+           return file;
            }
-     pipecmd=getUnpackCmd(fname); //also bam2fastx
-     this->openRead(fname.c_str(), pipecmd);
+     if (guess) {
+       pipecmd=guess_packer(fname);
+       if (!pipecmd.empty()) pipecmd.append(" -cd");
      }
+     else {
+       pipecmd=getUnpackCmd(fname);
+     }
+     if (pipecmd.empty()) {
+           //this->openRead(fname.c_str());
+           file=fopen(filename.c_str(), "r");
+           return file;
+           }
+     this->openRead(fname.c_str(), pipecmd);
+     return file;
+     }
+
+     FILE* openRead(const char* fname, std::string& popencmd);
+
+	 FILE* openRead(const char* fname) {
+	       std::string s(fname);
+	       return openRead(s);
+	       }
+	 FILE* openRead(const std::string fname, std::string& popencmd) {
+	   return this->openRead(fname.c_str(),popencmd);
+	   }
+
 
 	 FZPipe():filename(),pipecmd() {
 	   is_bam=false;
@@ -172,35 +238,46 @@ class FZPipe {
 	   }
 	 FZPipe(std::string& fname, std::string& pcmd):filename(fname),pipecmd(pcmd) {
 	   //open as a compressed file reader
+	   if (pipecmd.empty()) {
+		 this->openRead(fname);
+		 return;
+	   }
 	   is_bam=false;
 	   file=NULL;
 	   this->openRead(fname.c_str(), pipecmd);
 	   }
+
 	 void close() {
 	   if (file!=NULL) {
-		 if (pipecmd.empty()) fclose(file);
+		 if (is_bam) {
+	        samclose(bam_file);
+	        bam_file=NULL;
+		 }
+		 else {
+		   if (pipecmd.empty()) fclose(file);
 						 else pclose(file);
+		   }
 		 file=NULL;
 		 }
 	   }
 	 FILE* openWrite(const char* fname, std::string& popencmd);
 	 FILE* openWrite(const char* fname);
-	 FILE* openRead(const char* fname, std::string& popencmd);
 
-	 FILE* openRead(const char* fname);
-	 FILE* openRead(const std::string fname, std::string& popencmd) {
-	   return this->openRead(fname.c_str(),popencmd);
-	   }
-	 FILE* openRead(const std::string fname) {
-	   return this->openRead(fname.c_str());
-	   }
 	 void rewind();
+
+	 void seek(int64_t foffset)
+	 {
+	   if (is_bam) {
+		 bgzf_seek(bam_file->x.bam, foffset, SEEK_SET);
+	   }
+	   else {
+		 fseek(this->file, foffset, SEEK_SET);
+	   }
+	 }
 };
 
 void err_die(const char* format,...);
-
-//uint8_t* realloc_bdata(bam1_t *b, int size);
-//uint8_t* dupalloc_bdata(bam1_t *b, int size);
+void warn_msg(const char* format,...);
 
 class GBamRecord {
    bam1_t* b;
@@ -211,12 +288,15 @@ class GBamRecord {
    //    +qual
    //     +aux
    bool novel;
+
+   char tag[2];
+   uint8_t abuf[1024];
  public:
    GBamRecord(bam1_t* from_b=NULL) {
-      if (from_b==NULL) {
-           b=bam_init1();
-           novel=true;
-           }
+     if (from_b==NULL) {
+       b=bam_init1();
+       novel=true;
+     }
         else {
            b=from_b;
            novel=false;
@@ -252,6 +332,14 @@ class GBamRecord {
       b->core.flag=flags;
       }
 
+    void set_flag(uint16_t flag) { //use BAM_F* constants
+      b->core.flag |= flag;
+    }
+
+    void unset_flag(uint16_t flag) { //use BAM_F* constants
+      b->core.flag &= ~flag;
+    }
+
     //creates a new record from 1-based alignment coordinate
     //quals should be given as Phred33
     //Warning: pos and mate_pos must be given 1-based!
@@ -266,9 +354,14 @@ class GBamRecord {
     void add_quals(const char* quals); //quality values string in Phred33 format
     void add_aux(const char* str); //adds one aux field in plain SAM text format (e.g. "NM:i:1")
     void add_aux(const char tag[2], char atype, int len, uint8_t *data) {
+      //IMPORTANT:  strings (Z,H) should include the terminal \0
+      int addz=0;
+      if ((atype=='Z' || atype=='H') && data[len-1]!=0) {
+    	addz=1;
+        }
       int ori_len = b->data_len;
-      b->data_len += 3 + len;
-      b->l_aux += 3 + len;
+      b->data_len += 3 + len + addz;
+      b->l_aux += 3 + len + addz;
       if (b->m_data < b->data_len) {
         b->m_data = b->data_len;
         kroundup32(b->m_data);
@@ -276,15 +369,41 @@ class GBamRecord {
       }
       b->data[ori_len] = tag[0]; b->data[ori_len + 1] = tag[1];
       b->data[ori_len + 2] = atype;
+      if (addz) {
+    	b->data[ori_len+len+3]=0;
+        }
       memcpy(b->data + ori_len + 3, data, len);
       }
+   //--reading back aux tags:
+	 uint8_t* find_tag(const char tag[2]);
+	 //returns pointer at the beginning of tag data, or NULL if tag not found
+	 //the returned pointer can then be used by bam_aux2*() functions
+
+	 std::string tag_str(const char tag[2]); //return tag value for tag type 'Z'
+	 int tag_int(const char tag[2]); //return numeric value of tag (for numeric types)
+	 char tag_char(const char tag[2]); //return char value of tag (for type 'A')
+	 char spliceStrand(); // '+', '-' from the XS tag, or '.' if no XS tag
+	//--
+	 std::string qualities(); //return quality string, as is (ignores BAM_FREVERSE)
+	 std::string sequence();  //return read sequence as is (ignores BAM_FREVERSE)
+	 std::string seqData(std::string* readquals=NULL); //return seq and qv, reversed if BAM_FREVERSE
 };
 
 class GBamWriter {
    samfile_t* bam_file;
    bam_header_t* bam_header;
+   FILE* findex;
+   uint64_t wcount;
+   uint64_t idxcount;
+   int64_t idx_last_id;
+   bool external_header;
  public:
    void create(const char* fname, bool uncompressed=false) {
+	  findex=NULL;
+	  wcount=0;
+	  idxcount=0;
+	  idx_last_id=0;
+	  external_header=false;
       if (bam_header==NULL)
          err_die("Error: no bam_header for GBamWriter::create()!\n");
       if (uncompressed) {
@@ -297,14 +416,52 @@ class GBamWriter {
          err_die("Error: could not create BAM file %s!\n",fname);
       //do we need to call bam_header_write() ?
       }
+
+   void create(const char* fname, std::string& idxfile) {
+	  findex=NULL;
+	  wcount=0;
+	  idxcount=0;
+	  idx_last_id=0;
+	  external_header=false;
+      if (bam_header==NULL)
+         err_die("Error: no bam_header for GBamWriter::create()!\n");
+      bam_file=samopen(fname, "wb", bam_header);
+      if (bam_file==NULL)
+         err_die("Error: could not create BAM file %s!\n",fname);
+      if (!idxfile.empty()) {
+    	   findex = fopen(idxfile.c_str(), "w");
+    	   if (findex == NULL)
+    	     err_die("Error: cannot create file %s\n", idxfile.c_str());
+           }
+      }
+
    void create(const char* fname, bam_header_t* bh, bool uncompressed=false) {
+     findex=NULL;
+	 wcount=0;
+	 idxcount=0;
+	 idx_last_id=0;
+     external_header=false;
 	 bam_header=bh;
-	 create(fname,uncompressed);
+	 create(fname, uncompressed);
      }
 
    GBamWriter(const char* fname, bam_header_t* bh, bool uncompressed=false) {
       create(fname, bh, uncompressed);
+      external_header=true;
       }
+
+   GBamWriter(const char* fname, bam_header_t* bh, std::string& idxfile) {
+	  bam_header=bh;
+      create(fname, idxfile);
+      external_header=true;
+      }
+
+   GBamWriter(std::string& fname, std::string& idxfile) {
+	 //create BAM with empty header
+     external_header=false;
+	 bam_header=bam_header_init();
+	 create(fname.c_str());
+     }
 
    GBamWriter(const char* fname, const char* samfname, bool uncompressed=false) {
       tamFile samf_in=sam_open(samfname);
@@ -317,9 +474,23 @@ class GBamWriter {
       create(fname, uncompressed);
       }
 
+   GBamWriter(const char* fname, const char* samfname, std::string idxfile) {
+      tamFile samf_in=sam_open(samfname);
+      if (samf_in==NULL)
+         err_die("Error: could not open SAM file %s\n", samfname);
+      bam_header=sam_header_read(samf_in);
+      if (bam_header==NULL)
+         err_die("Error: could not read SAM header from %s!\n",samfname);
+      sam_close(samf_in);
+      create(fname, idxfile);
+      }
+
     ~GBamWriter() {
       samclose(bam_file);
-      bam_header_destroy(bam_header);
+      if (bam_header && !external_header)
+        bam_header_destroy(bam_header);
+      if (findex != NULL)
+        fclose(findex);
       }
    bam_header_t* get_header() { return bam_header; }
    int32_t get_tid(const char *seq_name) {
@@ -332,17 +503,23 @@ class GBamWriter {
    //given pos must be 1-based (so it'll be stored as pos-1 because BAM is 0-based)
    GBamRecord* new_record(const char* qname, const char* gseqname,
             int pos, bool reverse, const char* qseq, const char* cigar=NULL, const char* qual=NULL) {
+	 if (gseqname==NULL || strcmp(gseqname, "*")==0) {
+	   //probably an unmapped read
+	   //if (pos>0) err_die("Error: genomic position given for unmapped read!\n");
+	   return (new GBamRecord(qname, -1, 0, false, qseq, cigar, qual));
+	  }
+	 else {
       int32_t gseq_tid=get_tid(gseqname);
-      if (gseq_tid < 0 && strcmp(gseqname, "*")) {
+      if (gseq_tid < 0) {
             if (bam_header->n_targets == 0) {
                err_die("Error: missing/invalid SAM header\n");
                } else
                    fprintf(stderr, "Warning: reference '%s' not found in header, will consider it '*'.\n",
                                    gseqname);
             }
-
       return (new GBamRecord(qname, gseq_tid, pos, reverse, qseq, cigar, qual));
-      }
+	  }
+     }
 
    GBamRecord* new_record(const char* qname, int32_t flags, const char* gseqname,
          int pos, int map_qual, const char* cigar, const char* mgseqname, int mate_pos,
@@ -374,13 +551,63 @@ class GBamWriter {
       }
 
    void write(GBamRecord* brec) {
-      if (brec!=NULL)
-          samwrite(this->bam_file,brec->get_b());
+      if (brec!=NULL) {
+        samwrite(this->bam_file,brec->get_b());
+        wcount++;
       }
-   void write(bam1_t* b) {
-      samwrite(this->bam_file, b);
-      }
-};
 
+      }
+   void write(bam1_t* b, int64_t read_id=0) {
+     int64_t pre_block_addr=0; //offsets after last write()
+     int     pre_block_offs=0; //but before this write()
+     int64_t pre_pos=0;
+     bool write_index=false;
+     if (findex && read_id) {
+       if (idxcount >= INDEX_REC_COUNT && read_id != idx_last_id) {
+	 pre_pos = this->tell();
+	 pre_block_offs = pre_pos & 0xFFFF;
+	 pre_block_addr = (pre_pos >> 16) & 0xFFFFFFFFFFFFLL;
+	 write_index=true;
+       }
+       idx_last_id=read_id;
+       idxcount++;
+     }
+     
+     samwrite(this->bam_file, b);
+     wcount++;
+     if (write_index) {
+       int64_t offset = this->tell();
+       int     post_block_offs = offset & 0xFFFF; //offsets after this write()
+       int64_t post_block_addr = (offset >> 16) & 0xFFFFFFFFFFFFLL;
+       int data_len = b->data_len+BAM_CORE_SIZE;
+       if (post_block_addr != pre_block_addr &&
+	   post_block_offs>=data_len)
+	 //all data written in this block
+	 //WARNING: this check fails for very large BAM records (> 64K)
+	 {
+	   pre_pos = post_block_addr << 16;
+	 }
+
+       fprintf(findex, "%ld\t%ld\n", (long)read_id, (long)pre_pos);
+       idxcount = 0;
+     }
+   }
+   
+
+   int64_t tell() {
+     return bam_tell(this->bam_file->x.bam);
+   }
+
+
+   int64_t writtenCount() { return wcount; }
+
+   void flush() {
+     bgzf_flush(this->bam_file->x.bam);
+   }
+
+   void seek(int64_t offset) {
+     bam_seek(this->bam_file->x.bam, offset, SEEK_SET);
+   }
+};
 
 #endif
